fb9b8f138bd4e43ad672da51050aef8f706819f7
jkesh <jkeshi@gmail.com>
2022-10-26 17:34:16 -0400

    update status

--- scripts/parks.js
+++ scripts/parks.js
@@ -1,34 +1,44 @@
-
-
 const locationsDataArray = document.getElementById("locationDatalist");
+const nationalParkDataArray = document.getElementById("nationalParkList");
 
 function loadLocationDataList() {
-   //collection
-    locationsArray.forEach((location) => {
-      let option = new Option(location, location);
-      locationDataList.appendChild(option);
-    });
+  //collection
+  locationsArray.forEach((location) => {
+    let option = new Option(location, location);
+    locationDataList.appendChild(option);
+  });
 }
 
 window.onload = () => {
   loadLocationDataList();
+};
+
+function buildLocationDataRow(tableBody, nationalPark) {
+  let row = tableBody.insertRow(-1);
+  let cell1 = row.insertCell(0);
+  cell1.innerText = nationalPark.Name;
+  let cell2 = row.insertCell(1);
+  cell1.innerText = nationalPark.Address;
+  let cell3 = row.insertCell(2);
+  cell1.innerText = nationalPark.City;
+  let cell4 = row.insertCell(3);
+  cell1.innerText = nationalPark.State;
+  let cell5 = row.insertCell(4);
+  cell1.innerText = nationalPark.zip;
+  let cell6 = row.insertCell(5);
+  cell1.innerText = nationalPark.phone;
 }
- 
-function buildLocationDataList(location) {
-  let table = document.createElement("table");
-    
-  }
+// function buildLocationDataList(table, ) {
+//   let table = document.createElement("table");
+//     let locationsArrayRow = document.createElement (`tr`);
 
-
-// const nationalParkDataArray = document.getElementById("nationalParkList");
+//   }
 
 // function loadNationalParkList(params) {}
-
 
 // const parkTypesDataArray = document.getElementById("parkDataList");
 
 // function loadParkDataList() {}
-
 
 // const mountainsDataArray = document.getElementById("moutainsDataArrayList");
 
